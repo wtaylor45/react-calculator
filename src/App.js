a58408b5f9f8d33cd5e73174006b245cbd4826d8
Will Taylor <taylorw@udel.edu>
2019-02-10 19:28:04 -0500

    Layout of UI in place

--- src/App.js
+++ src/App.js
@@ -1,28 +1,15 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import styled from 'styled-components';
+import { Calculator } from './containers/Calculator';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const Container = styled.div`
+  display: grid;
+  place-content: center;
+  place-items: center;
+  height: 100%;
+`
 
-export default App;
+export const App = () =>
+  <Container className="App">
+    <Calculator />
+  </Container>
