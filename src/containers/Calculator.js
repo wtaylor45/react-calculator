c83dbfac321c3387f2fe8e0b948c82f2b0d7513c
Will Taylor <taylorw@udel.edu>
2019-02-11 18:54:45 -0500

    Evaluating calculations working

--- src/containers/Calculator.js
+++ src/containers/Calculator.js
@@ -4,6 +4,7 @@ import { Display } from '../presentational/Display';
 import { Button } from '../presentational/Button';
 import { Buttons } from '../presentational/Buttons';
 import { Grid, GridItem } from '../presentational/Grid';
+import math from 'mathjs';
 
 const Container = styled.div`
     width: 50vmin;
@@ -16,40 +17,48 @@ const Container = styled.div`
 `
 
 export const Calculator = (props) => {
-    const [operations, setOperations] = useState([]);
-    const [total, setTotal] = useState(0);
+    const [operations, setOperations] = useState('');
 
     const addToOperations = (e) => {
-        if(operations.length < 12)
-            setOperations([...operations, e.target.value]);
+        switch(e.target.value){
+            case 'C':
+                setOperations('');
+                break;
+            case '=':
+                setOperations(math.eval(operations).toString());
+                break;
+            default:
+                setOperations(operations.concat(e.target.value));
+                break;
+        }
     }
 
     return (
         <Container>
-            <Display height="30%">{operations.join('')}</Display>
-            <Buttons onClick={addToOperations} height="70%">
-                <Grid columnTemplate="auto auto auto auto" rowTemplate="auto auto auto auto auto">
-                    <Button fullWidth value={"C"} category="modifier"></Button>
-                    <Button fullWidth value={"-"} category="modifier"></Button>
-                    <Button fullWidth value={"%"} category="modifier"></Button>
-                    <Button fullWidth value={"/"} category="operator"></Button>
+            <Display height='30%'>{operations}</Display>
+            <Buttons onClick={addToOperations} height='70%'>
+                <Grid columnTemplate='auto auto auto auto' rowTemplate='auto auto auto auto auto'>
+                    <Button fullWidth value={'C'} category='modifier'></Button>
+                    <Button fullWidth value={'-'} category='modifier'></Button>
+                    <Button fullWidth value={'%'} category='modifier'></Button>
+                    <Button fullWidth value={'/'} category='operator'></Button>
                     <Button fullWidth value={'7'}></Button>
                     <Button fullWidth value={'8'}></Button>
                     <Button fullWidth value={'9'}></Button>
-                    <Button fullWidth value={"*"} category="operator"></Button>
-                    <Button fullWidth value={"4"}></Button>
+                    <Button fullWidth value={'*'} category='operator'></Button>
+                    <Button fullWidth value={'4'}></Button>
                     <Button fullWidth value={'5'}></Button>
-                    <Button fullWidth value={"6"}></Button>
-                    <Button fullWidth value={"-"} category="operator"></Button>
+                    <Button fullWidth value={'6'}></Button>
+                    <Button fullWidth value={'-'} category='operator'></Button>
                     <Button fullWidth value={'1'}></Button>
                     <Button fullWidth value={'2'}></Button>
                     <Button fullWidth value={'3'}></Button>
-                    <Button fullWidth value={"+"} category="operator"></Button>
-                    <GridItem colSpan="2">
+                    <Button fullWidth value={'+'} category='operator'></Button>
+                    <GridItem colSpan='2'>
                         <Button fullWidth value={'0'}></Button>
                     </GridItem>
                     <Button fullWidth value={'.'}></Button>
-                    <Button fullWidth value={'='} category="operator"></Button>
+                    <Button fullWidth value={'='} category='operator'></Button>
                 </Grid>
             </Buttons>
         </Container>
